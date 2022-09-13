03e8042ceb82797f3dd185df41eabc59d6bd2d8c
Deryn Brown <Deryn.2017@gmail.com>
2022-09-13 08:26:30 -0500

    commit tree d1aa2b37c03c4cc88b356d1817e5dfecf9153eb4

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const initialValue = 0;
+const totalBatteries = batteryBatches.reduce(function(total, batches) {
+    return batches + total
+});
+
+// function getTotalAmountForProducts(products) {
+//     let totalPrice = 0;
+  
+//     for (const product of products) {
+//       totalPrice += product.price;
+//     };
+  
+//     return totalPrice;
+//   }
+  
+//   console.log(getTotalAmountForProducts(products)); 
